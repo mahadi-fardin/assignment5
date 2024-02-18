564ce0ac51b9f834ee63ea1388eb19c5078dfbb0
mahadi-fardin <mahadihasan50200@gmail.com>
2024-02-18 21:08:38 +0600

    addsdd to seat and js

---
+++ indiex.js
@@ -0,0 +1,11 @@
+
+
+const cards =document.querySelectorAll(".seat");
+
+console.log(cards);
+
+for (let index = 0; index < cards.length; index++ ){
+    const element = cards[index];
+
+    console.log(element);
+}
